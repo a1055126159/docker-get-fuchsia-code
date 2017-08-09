aac3c2b9de5392a15f3a5586ec74f0f6a3aab066
root <root@DESKTOP-M1BRDNE.localdomain>
2017-08-10 00:59:43 +0800

    Third

--- get-source-code.sh
+++ get-source-code.sh
@@ -3,7 +3,7 @@ df -h
 echo "Preparing Environment (Step 1/5)"
 sudo curl https://raw.githubusercontent.com/laodcn/hosts/master/hosts > /etc/hosts
 sudo apt-get install golang git libcurl4-openssl-dev build-essential curl unzip -y
-curl https://index-a1055126159.c9users.io/git/git-openssl/git_1.9.1-1ubuntu0.5_amd64.deb > git_1.9.1-1ubuntu0.5_amd64.deb
+curl https://raw.githubusercontent.com/a1055126159/docker-get-fuchsia-code/master/git_1.9.1-1ubuntu0.5_amd64.deb > git_1.9.1-1ubuntu0.5_amd64.deb
 dpkg -i git_1.9.1-1ubuntu0.5_amd64.deb
 cd ~
 curl -s https://raw.githubusercontent.com/fuchsia-mirror/jiri/master/scripts/bootstrap_jiri | bash -s fuchsia
