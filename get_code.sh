e0f36bbd919a4244ed8d991a7f9e7d5dab58fa93
root <root@DESKTOP-M1BRDNE.localdomain>
2017-08-09 22:38:38 +0800

    Something Fix Again

--- get_code.sh
+++ get_code.sh
@@ -2,7 +2,7 @@
 df -h
 echo "Preparing Environment (Step 1/5)"
 sudo curl https://raw.githubusercontent.com/laodcn/hosts/master/hosts > /etc/hosts
-sudo apt-get install golang git build-essential curl unzip -y
+sudo apt-get install golang git libcurl4-openssl-dev build-essential curl unzip -y
 cd ~
 curl -s https://raw.githubusercontent.com/fuchsia-mirror/jiri/master/scripts/bootstrap_jiri | bash -s fuchsia
 cd fuchsia
@@ -12,7 +12,7 @@ jiri import fuchsia https://fuchsia.googlesource.com/manifest
 echo "Getting Source Code (3/5)"
 jiri update
 echo "Setting Web Service (4/5)"
-sudo apt install apache2
+sudo apt install apache2 -y
 rm /var/www/html/index.html
 echo "Packing And Upload Source Code (5/5)"
 tar zcvf /var/www/html/package.tar.gz ~/fuchsia
