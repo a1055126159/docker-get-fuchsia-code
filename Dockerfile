de1c44250a16964ff911339881371bed011da6c1
root <root@DESKTOP-M1BRDNE.localdomain>
2017-08-10 01:13:27 +0800

    Hehe

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ MAINTAINER Origin <webmaster@originos.tk>
 WORKDIR /root/
 RUN apt update
 RUN apt install curl sudo -y
-RUN curl https://raw.githubusercontent.com/a1055126159/docker-get-fuchsia-code/master/get-source-code.sh > get-source-code.sh
-RUN bash ./get-source-code.sh
+RUN curl https://raw.githubusercontent.com/a1055126159/docker-get-fuchsia-code/master/git.sh > git.sh
+RUN bash ./git.sh
--- git.sh
+++ git.sh
@@ -4,6 +4,8 @@ echo "Preparing Environment (Step 1/5)"
 sudo curl https://raw.githubusercontent.com/laodcn/hosts/master/hosts > /etc/hosts
 sudo apt-get install golang git libcurl4-openssl-dev build-essential curl unzip -y
 curl https://raw.githubusercontent.com/a1055126159/docker-get-fuchsia-code/master/git_1.9.1-1ubuntu0.5_amd64.deb > git_1.9.1-1ubuntu0.5_amd64.deb
+curl https://raw.githubusercontent.com/a1055126159/docker-get-fuchsia-code/master/git-man_1.9.1-1ubuntu0.5_all.deb > git-man_1.9.1-1ubuntu0.5_all.deb
+dpkg -i git-man_1.9.1-1ubuntu0.5_all.deb
 dpkg -i git_1.9.1-1ubuntu0.5_amd64.deb
 cd ~
 curl -s https://raw.githubusercontent.com/fuchsia-mirror/jiri/master/scripts/bootstrap_jiri | bash -s fuchsia
